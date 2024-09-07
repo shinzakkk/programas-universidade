42381bf82bd701d5a352248db1579122614de18d
shinzakkk <120679867+shinzakkk@users.noreply.github.com>
2024-09-06 21:52:19 -0300

    vetores0409.2.cpp
    programa que recebe matrizes de 2 a 300 linhas e colunas, dando a opção de imprimir sua diagonal principal, sua matriz transposta, seus elementos multiplicados por algum escalar ou seus elementos somados por algum escalar.
    Exibido em formato matricial com até duas casas decimais de precisão.

--- vetores0409.2.cpp
+++ vetores0409.2.cpp
@@ -1,32 +1,74 @@
 #include <stdio.h>
 int main(){
-	int m, n, i, j, a;
+	int m, n, i, j, opcao;
+	float escalar;
 	do{
-		printf("insira os valores de linhas e de colunas da matriz: ");
-		scanf("%d %d", &m, &n);
+		printf("insira a quantidade de linhas na matriz: ");
+		scanf("%d", &m);
+		printf("insira a quantidade de colunas na matriz: ");
+		scanf("%d", &n);
 	}while(m < 2 || m > 300 || n < 2 || n > 300);
-	int matriz[m][n];
+	float matriz[m][n];
 	
-	for(i=1; i <= m; i++){
-		for(j=1; j <= n; j++){
-			printf("digite o valor do elemento na posicao %d, %d na matriz: ", i, j);
-			scanf("%d", matriz[m][n]);
-		}
+	for(i=0; i<m; i++){
+		for(j=0; j<n; j++){
+			printf("insira o elemento %d,%d da matriz: ", i+1, j+1);
+			scanf("%f", &matriz[i][j]);
+		}	
 	}
-	
+
 	do{
-		printf("aperte 1 para imprimir a diagonal principal\naperte 2 para imprimir a matriz transposta\naperte 3 para imprimir a multiplicacao dos elemntos da matriz por uma escalar\naperte 4 para imprimir a soma dos elementos de uma matriz com um escalar\n");
-		scanf("%d", &a);
-	}while(a > 4);
-	if(a = 1){
-		i = 0;
-		j = 0;
-		while (m > i && n > j){
-			printf("%d ", matriz[m][n]);
-			i++;
-			j++;
-		}
+		printf("aperte 1 para imprimir a diagonal principal da sua matriz\n");
+		printf("aperte 2 para imprimir a matriz transposta de sua matriz\n");
+		printf("aperte 3 para imprimir a sua matriz com os elementos multiplicados por um escalar\n");
+		printf("aperte 4 para imprimir a sua matriz com os elementos somados a um escalar\n");
+		printf("aperte 5 para sair\n");
+		scanf("%d", &opcao);
+	switch(opcao){
+		case 1:
+			for(i=0; i<m; i++){
+				for(j=0; j<n; j++){
+					if(i==j){
+						printf("%.2f ", matriz[i][j]);
+					} else {
+						printf("0 ");
+					}
+				}
+				printf("\n");
+			}
+			break;
+		case 2:
+			for(j=0; j<n; j++){
+				for(i=0; i<m; i++){
+					printf("%.2f ", matriz[i][j]);
+				}
+				printf("\n");
+			}
+			break;
+		case 3:
+			printf("digite o valor que voce deseja multplicar com os elementos de sua matriz: ");
+			scanf("%f", &escalar);
+			for(i=0; i<m; i++){
+				for(j=0; j<n; j++){
+					printf("%.2f ", escalar * matriz[i][j]);
+				}
+				printf("\n");
+			}
+			break;
+		case 4:
+			printf("digite o valor que voce deseja somar aos elementos de sua matriz: ");
+			scanf("%f", &escalar);
+			for(i=0; i<m; i++){
+				for(j=0; j<n; j++){
+					printf("%.2f ", escalar + matriz[i][j]);
+				}
+				printf("\n");
+			}
+			
+		default:
+			printf("saindo. . .\n");
+			break;
 	}
+	}while(opcao > 6 );
 	return 0;
 }
-
