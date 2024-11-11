fb26b837cca47b5e7907bcd3a3a564ac65fa23e9
shinzakkk <120679867+shinzakkk@users.noreply.github.com>
2024-11-11 12:40:27 -0600

    Update personagem2.h

--- personagem2.h
+++ personagem2.h
@@ -14,11 +14,23 @@ typedef struct{
 	int ouro; //dinheiro
 } Persona;
 
+typedef struct{
+	int hpPotion;
+	int mpPotion;
+	int atkBooster;
+	int defBooster;
+	int agiBooster;
+}Consumiveis;
+
+
 void personagem();
 void criarPersonagem(Persona *MC, FILE *mcArquivo);
 void statusPersonagem(Persona *MC, FILE *mcArquivo);
 void upPersonagem(Persona *MC, int pts, FILE *mcArquivo);
 void deletePersonagem(Persona *MC, FILE *mcArquivo);
 void comprarItem(Persona *MC);
-
-#endif
+void inventario();
+void comprarItem(Consumiveis *itens);
+void viewInventario(Consumiveis *itens);
+void descartarItem(Persona *MC);
+#endi
